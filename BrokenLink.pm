4e71874cb155e6dadae0efe5c213cdae0e808ec1
Samuel <samuelgomezcrespo@gmail.com>
2012-11-17 18:10:42 +0100

    First commit

---
+++ BrokenLink.pm
@@ -0,0 +1,13 @@
+=pod
+This is going to be a Perl port of mod_brokenlink
+http://code.google.com/p/modbrokenlink/
+which is written in C.
+
+Examples of mod_perl handlers:
+http://perl.apache.org/docs/2.0/user/handlers/intro.html
+http://modperlbook.org/code/chapters/ch25-next_generation/Book/Eliza2.pm
+http://cpan-search.sourceforge.net/Apache2/DocServer.pm.html
+https://svn.apache.org/repos/asf/spamassassin/branches/check_plugin/spamd-apache2/lib/Mail/SpamAssassin/Spamd/Apache2/Config.pm
+http://perl.apache.org/docs/2.0/user/handlers/http.html#PerlLogHandler
+
+=cut
