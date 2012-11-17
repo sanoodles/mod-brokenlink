91fc3e18394a1ea5116d948d12032cce68dc1ea1
Samuel <samuelgomezcrespo@gmail.com>
2012-11-17 18:29:40 +0100

    Constants

--- BrokenLink.pm
+++ BrokenLink.pm
@@ -11,3 +11,29 @@ https://svn.apache.org/repos/asf/spamassassin/branches/check_plugin/spamd-apache
 http://perl.apache.org/docs/2.0/user/handlers/http.html#PerlLogHandler
 
 =cut
+
+use constant {
+  MBL_FALSE => 0,
+  MBL_TRUE => 1,
+  DEF_SOCK_TIMEOUT => 30 * (10 ** 6),
+  CRLF_STR => "\r\n",
+  TIME_FORMAT => "%Y-%m-%d %H:%M:%S",
+  TIME_FORMAT_ISO => "%Y-%m-%dT%H:%M:%S%z",
+  MODULE => "mod_brokenlink",
+  URISIZE => 1024
+}
+
+# debugging
+use constant {
+  MBL_DEBUG_MODE => MBL_FALSE,
+  DEBUG => MBL_DEBUG_MODE
+}
+
+# sending
+use constant {
+  MBL_NOTIFY_FILENAME => "/mod_brokenlink_notify",
+  MBL_NOTIFY_REFLEXIVE => MBL_FALSE,
+  MBL_USER_AGENT => "Apache mod_brokenlink"
+}
+
+
